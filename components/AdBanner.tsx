d3674b6bf375777bb484f406428badf83777f9b7
Sheeza1234 <https://github.com/Sheeza1234>
2025-05-29 17:43:33 +0500

    new

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,9 +1,21 @@
 // AdBanner.tsx
 import React from 'react';
-import { Platform } from 'react-native';
-import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import { Platform, View } from 'react-native';
+
+let BannerAd: any, BannerAdSize: any, TestIds: any;
+if (Platform.OS !== 'web') {
+  const googleAds = require('react-native-google-mobile-ads');
+  BannerAd = googleAds.BannerAd;
+  BannerAdSize = googleAds.BannerAdSize;
+  TestIds = googleAds.TestIds;
+}
 
 const AdBanner: React.FC = () => {
+  if (Platform.OS === 'web') {
+    // Optionally show a placeholder on web
+    return <View style={{ height: 50 }} />;
+  }
+
   const adUnitId = Platform.select({
     ios: 'ca-app-pub-7230381744219330/3300187380',
     android: 'ca-app-pub-7230381744219330/1959185553',
@@ -14,9 +26,9 @@ const AdBanner: React.FC = () => {
       unitId={__DEV__ ? TestIds.BANNER : adUnitId!}
       size={BannerAdSize.FULL_BANNER}
       requestOptions={{
-        requestNonPersonalizedAdsOnly: false, // set to true if needed for GDPR
+        requestNonPersonalizedAdsOnly: false,
       }}
-      onAdFailedToLoad={(error) => {
+      onAdFailedToLoad={(error: any) => {
         console.log('Ad load error:', error);
       }}
     />
